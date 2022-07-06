303845a98e319fc6c6c89717d5637b2935451e98
rashmitalwadker <rashmitalwadker@gmail.com>
2022-07-06 11:22:08 +0530

    my react app

---
+++ src/Home.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const Home = () => {
+
+    return ( <
+        div className = "home" >
+        <
+        h1 > Summer Art Camp!5 days of Artists and monet, Van Ghosh Mattise & More < /h1> <
+        li style = {
+            { marginRight: "400px" }
+        } > multi day course < i className = 'sym' > ? < /i > < /li > <
+        div className = "content-para" >
+        <
+        ul className = 'page' >
+        <
+        section > < li > In this 5 day class we will explore artis Monet, Mattise,
+        van Ghosh, < br / > among others and then recreate painting using crayons and waterColor. < /li><li>
+
+        Students have fun learning about artists & creating their own art inspired by their work. < /li> < div > <
+        li style = {
+            { color: "yellow" }
+        } > <
+        i class = "fa-solid fa-star" > < /i>  <
+        i class = "fa-solid fa-star" > < /i>  <
+        i class = "fa-solid fa-star" > < /i>  <
+        i class = "fa-solid fa-star" > < /i>  <i
+        i class = "fa-solid fa-star" > < /i>  <
+        span className = "tag1"
+        style = {
+            { color: "black" }
+        } > 467 total reviews
+        for learners < /
+        span > < /
+        li > < div > <
+        li style = {
+            { color: "yellow" }
+        } >
+        <
+        i class = "fa-solid fa-star" > < /i>  <
+        i class = "fa-solid fa-star" > < /i> < i class = "fa-solid fa-star" > < /i >
+        <
+        i class = "fa-solid fa-star" > < /i> < i class = "fa-solid fa-star" > < /i >
+        <
+        i class = "fa-solid fa-star" > < /i> <span style={{color:"black"}}>
+        5 reviews
+        for this class < / span > </li > < /div> < /
+        div >
+        <
+        div >
+        <
+        button className = "btn"
+        style = {
+            {
+                backgroundColor: "skyBlue",
+                border: "2px solid black",
+                borderRadius: "20px",
+                padding: "12px",
+                width: "300px",
+                marginTop: "20px",
+                fontSize: "1rem",
+                fontWeight: "2rem",
+                cursor: "pointer"
+            }
+        } > See Class Schedule { '>' } < /button >
+
+        <
+        /div>
+
+
+        <
+        /
+        section >
+
+        <
+        img src = "image/1.png"
+
+        className = "imd"
+        alt = "
+        " / >
+
+        <
+        /ul>
+
+
+        <
+        /div>
+
+
+
+        <
+        /
+        div >
+    )
+}
+
+export default Home
